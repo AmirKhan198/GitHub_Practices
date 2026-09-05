a223995a0231f76a505ebadf6017ce74b53935e4
Amirkhan198 <amirk198786@gmail.com>
2026-09-05 11:37:33 +0530

    Fix bug

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hi Amir khan");
 
-Console.WriteLine("Hi i am muskan khan")
+Console.WriteLine("Hi i am muskan khan");
